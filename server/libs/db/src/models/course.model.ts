da1999a74ac6db7dc9edee0b1817327423d155d0
uctoo-liut <2394630102@qq.com>
2021-05-19 22:48:14 +0800

    update

---
+++ server/libs/db/src/models/course.model.ts
@@ -0,0 +1,18 @@
+import { modelOptions, prop, Ref } from '@typegoose/typegoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Episode } from './episode.model';
+@modelOptions({
+  schemaOptions: {
+    timestamps: true,
+  },
+})
+export class Course {
+  @ApiProperty({ description: '课程名称' })
+  @prop()
+  name: string;
+  @ApiProperty({ description: '封面图' })
+  @prop()
+  cover: string;
+  @prop({ itemsRef: 'Episode' })
+  episodes: Ref<Episode>[];
+}
---
+++ server/libs/db/src/models/episode.model.ts
@@ -0,0 +1,15 @@
+import { modelOptions, prop } from '@typegoose/typegoose';
+import { ApiProperty } from '@nestjs/swagger';
+@modelOptions({
+  schemaOptions: {
+    timestamps: true,
+  },
+})
+export class Episode {
+  @ApiProperty({ description: '课时名称' })
+  @prop()
+  name: string;
+  @ApiProperty({ description: '课时文件' })
+  @prop()
+  file: string;
+}

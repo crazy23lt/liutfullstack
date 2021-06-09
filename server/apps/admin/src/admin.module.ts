ddbecd1c41ccb0361267fa828ddf1b2a7fac6756
crazy23lt <2394630102@qq.com>
2021-06-09 15:42:27 +0800

    环境变量完结

--- server/apps/admin/src/admin.module.ts
+++ server/apps/admin/src/admin.module.ts
@@ -1,39 +1,43 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
-import { DbModule } from '@libs/db';
 import { UsersModule } from './users/users.module';
 import { CoursesModule } from './courses/courses.module';
 import { EpisodesModule } from './episodes/episodes.module';
 import { MulterModule } from '@nestjs/platform-express';
+import { CommonModule } from '@libs/common';
 const MAO = require('multer-aliyun-oss');
 @Module({
   imports: [
-    // ↓ 文件存储 ↓
-    MulterModule.register({
-      // ↓ 存储到阿里云oss ↓
-      storage: MAO({
-        config: {
-          region: 'oss-cn-shenzhen',
-          accessKeyId: 'LTAI5tEi76UD6mLms1mGr8QS',
-          accessKeySecret: 'ZEg9jAun051YP5fEL9HEdl9fm41a37',
-          bucket: 'liutfullstack',
-        },
-        filename: function (req, file, cb) {
-          cb(
-            null,
-            file.originalname +
-              '-' +
-              Date.now() +
-              '.' +
-              file.mimetype.match(/\/(\S*)/)[1],
-          );
-        },
-      }),
-      // ↓ （存储到本地）目标文件夹 ↓
-      // dest: 'uploads',
+    // ↓ 文件存储 异步 ↓
+    MulterModule.registerAsync({
+      useFactory() {
+        return {
+          // ↓ 存储到阿里云oss ↓
+          storage: MAO({
+            config: {
+              region: process.env.REGION,
+              accessKeyId: process.env.ACCESSKEYID,
+              accessKeySecret: process.env.ACCESSKEYSECRET,
+              bucket: process.env.BUCKET,
+            },
+            filename: function (req, file, cb) {
+              cb(
+                null,
+                file.originalname +
+                  '-' +
+                  Date.now() +
+                  '.' +
+                  file.mimetype.match(/\/(\S*)/)[1],
+              );
+            },
+          }),
+          // ↓ （存储到本地）目标文件夹 ↓
+          // dest: 'uploads',
+        };
+      },
     }),
-    DbModule,
+    CommonModule,
     UsersModule,
     CoursesModule,
     EpisodesModule,
